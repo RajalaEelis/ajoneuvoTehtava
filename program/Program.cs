531da3b32655a052359521c791f2302eca8fc2ee
EELIS RAJALA <eelis.rajala@edu.sakky.fi>
2022-09-14 12:50:00 +0300

    Add project files.

---
+++ program/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo auto1 = new Ajoneuvo();
+            Ajoneuvo auto2 = new Ajoneuvo();
+            Ajoneuvo auto3 = new Ajoneuvo();
+            Ajoneuvo auto4 = new Ajoneuvo();
+
+            List<Ajoneuvo> autot = new List<Ajoneuvo>();
+
+            string input;
+            int aktiivinenAuto = 0;
+            bool lopeta = false;
+
+            while (!lopeta)
+            {
+
+             Console.WriteLine("T <= Tankkaa ajoneuvoa");
+             Console.WriteLine("A <= Aja ajoneuvolla");
+                Console.WriteLine("L <= Lopeta sovellus");
+                Console.WriteLine("U <= lisää ajoneuvo");
+             Console.WriteLine("valitse toiminto");
+             input = Console.ReadLine();
+             switch (input)
+             {
+                case "T":
+                    auto1.Tankkaus();
+                    break;
+                case "A":
+                    Console.WriteLine("montako kilometriä ajetaan?");
+                    input = Console.ReadLine();
+                    auto1.Ajo(double.Parse(input));
+                    break;
+                    case "U":
+                        autot.Add(new Ajoneuvo());
+                        break;
+                    case "L":
+                        lopeta = true;
+                        break;
+                default:
+                    break;
+             }
+            }
+        }
+    }
+}
---
+++ program/ajoneuvo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program
+{
+    class Ajoneuvo
+    {
+        
+        public double Polttoaineenmaara { get; set; }
+        public double Polttoainetankki { get; set; }
+        public double Keskikulutus { get; set; }
+
+        public Ajoneuvo()
+        {
+            Polttoaineenmaara = 5;
+            Polttoainetankki = 50;
+            Keskikulutus = 7.8;
+        }
+
+        public void Tankkaus()
+        {
+            double polttoaineenHinta = 1.88;
+
+            double puuttuvaPolttoaine = Polttoainetankki - Polttoaineenmaara;
+            puuttuvaPolttoaine = Math.Truncate(puuttuvaPolttoaine * 100) / 100;
+            Console.WriteLine("Polttoainetta tankattu: " + $"{puuttuvaPolttoaine}");
+            Polttoaineenmaara = Polttoainetankki;
+
+            double hinta = puuttuvaPolttoaine * polttoaineenHinta;
+            hinta = Math.Truncate(hinta * 100) / 100;
+            Console.WriteLine($"Polttoaineen hinta: {hinta} ");
+
+
+
+
+        }
+        public void Ajo(double ajetutkilometrit)
+        {
+            double Kulunutpolttoaine = ajetutkilometrit / 100 * Keskikulutus;
+           
+            Kulunutpolttoaine = Math.Truncate(Kulunutpolttoaine * 100) / 100;
+            Console.WriteLine($"kuluneen polttoaineen määrä: {Kulunutpolttoaine} ");
+            Polttoaineenmaara = Polttoaineenmaara - Kulunutpolttoaine;
+            Polttoaineenmaara = Math.Truncate(Polttoaineenmaara * 100) / 100;
+            Console.WriteLine($"Polttoainetta jäljellä: {Polttoaineenmaara}");
+
+
+
+
+           // Ajoneuvo.Ajo(parametri);
+           //kaava: kilometrit / 100* keskikulutus
+            //Console.WriteLine("tämä on parametri");
+            //Ajoneuvo auto1 = new Ajoneuvo();
+            //auto1.Ajo(parametri);
+            //auto1.Ajo(35.6);
+        } 
+
+        
+
+
+
+
+
+
+    }
+}
